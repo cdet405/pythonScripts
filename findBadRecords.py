f8cc53e2d75c091d76e756c026c6a047a76c1174
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-05-31 17:00:16 -0400

    Update findBadRecords.py
    improved output

--- findBadRecords.py
+++ findBadRecords.py
@@ -1,4 +1,4 @@
-# Locate Schema Breaking Records 
+# Locate Schema Breaking Records
 import json
 
 with open('nld-tickets1685558410.ndjson', "r") as source_file:
@@ -6,7 +6,8 @@ with open('nld-tickets1685558410.ndjson', "r") as source_file:
     for line in source_file:
         json_line = json.loads(line)
         if ("customer" in json_line and json_line["customer"] is not None
-            and "meta" in json_line["customer"] and json_line["customer"]["meta"] is not None
-            and "profile_picture_url" in json_line["customer"]["meta"]):
-            print(f"Found 'customer.meta.profile_picture_url' in line {line_number}: {json_line['customer']['meta']['profile_picture_url']}")
+                and "meta" in json_line["customer"] and json_line["customer"]["meta"] is not None
+                and "profile_picture_url" in json_line["customer"]["meta"]):
+            print(f"Found 'customer.meta.profile_picture_url' in line {line_number}:\
+             [id:{json_line['id']}][ts:{json_line['created_datetime']}")
         line_number += 1
