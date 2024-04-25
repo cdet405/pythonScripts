c4f3db78098b4fb27f1b131ffcfaae2faf6fcf53
cdet405 <58343544+cdet405@users.noreply.github.com>
2024-04-25 17:54:04 -0400

    Update txt2csv.py

--- txt2csv.py
+++ txt2csv.py
@@ -3,6 +3,8 @@
 import os 
 import pandas as pd 
 
+# TODO: clean this up, add func to accept just 1 file
+
 file = 'export.txt'
 path = 'C:\\Users\\Me\\rawFiles'
 filepath = os.path.join(path, file)
@@ -15,3 +17,20 @@ df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 df.to_csv(outfilepath, index=False)
 
 print('done')
+
+def prep_files(path_in, path_out):
+  files = os.listdir(path_in)
+  for i in files:
+    output = os.path.join(path_out, f"processed_{i}")
+    try:
+      df = pd.read_csv(os.path.join(path_in, i), delimiter="\t")
+      df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+      df.to_csv(output, index=False)
+      print('Success: %s -> %s' % (i, output)
+    except Exception as e:
+      print('%s Failed With Error: %s' % (i, e)
+      print('\nSkipping to next file.)
+      pass
+
+if __name__ == '__main__':
+    prep_files(path_in='thepathtolookin',path_out='wheretosave')
